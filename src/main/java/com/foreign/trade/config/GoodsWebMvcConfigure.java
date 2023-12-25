48f205a8f1bd4e214889b26748bf4f12d8b5201e
Eunyn <3197689838@qq.com>
2023-12-25 21:49:52 +0800

    pass through the mall path

--- src/main/java/com/foreign/trade/config/GoodsWebMvcConfigure.java
+++ src/main/java/com/foreign/trade/config/GoodsWebMvcConfigure.java
@@ -25,7 +25,7 @@ public class GoodsWebMvcConfigure implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(goodsAdminInterceptor)
                 .addPathPatterns("/admin/**")
-                .addPathPatterns("/mall/**")
+//                .addPathPatterns("/mall/**")
                 .excludePathPatterns("/admin/login")
                 .excludePathPatterns("/admin/css/*")
                 .excludePathPatterns("/admin/fonts/*")
